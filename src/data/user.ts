37d8a15d19eea4514bcd595e464600ca79aaf178
Mariem Tijani <mariemtijani60@gmail.com>
2025-08-30 14:42:12 +0100

    first commit

---
+++ src/data/user.ts
@@ -0,0 +1,49 @@
+export const USER = {
+  firstName: "Meriem ",
+  lastName: "Tijani",
+  displayName: "Meriem Tijani",
+  username: "MeriemTijani",
+  gender: "female",
+  bio: "Creating with code, driven by passion.",
+  flipSentences: [
+    "Software Developer",
+    "Web Designer",
+    "Open Source Contributor",
+  ],
+  address: "Tangier, Morocco",
+  phoneNumber: "KzIxMjYyMDAyMTA5OA==", // E.164 format, base64 encoded (https://t.io.vn/base64-string-converter)
+  email: "bWFyaWVtdGlqYW5pNjBAZ21haWwuY29t", // base64 encoded
+  website: "https://chanhdai.com",
+  otherWebsites: [
+   
+  ],
+  dateOfBirth: "1998-10-10", // YYYY-MM-DD
+  jobTitle: "Software Developer",
+  jobs: [
+    {
+      title: "Senior Frontend Developer & UI Design Lead",
+      company: "Simplamo",
+      website: "https://simplamo.com?ref=IN-926722",
+    },
+    {
+      title: "Founder",
+      company: "Quaric",
+      website: "https://quaric.com",
+    },
+  ],
+  about: `
+Hello, World! I’m Meriem Tijani — a dedicated Full Stack Developer with a strong passion for building complete and efficient web applications that not only look great but also perform seamlessly.
+
+I work across both the frontend and backend, blending creativity with logic to develop user-friendly interfaces and powerful backend systems. I enjoy crafting experiences that are intuitive and engaging, while also making sure the structure behind the scenes is clean, reliable, and scalable.
+
+Whether it's designing the flow of a user interface or implementing the logic that powers it, I take pride in delivering work that is thoughtful, well-structured, and focused on real-world impact. Every project is an opportunity for me to grow, explore new ideas, and bring meaningful digital solutions to life.
+
+I'm always motivated to improve and take on new challenges, constantly learning and refining my approach to become the best developer I can be. My goal is to build high-quality applications that make a difference.
+  `,
+  avatar: "https://assets.chanhdai.com/images/chanhdai-avatar-ghibli.jpeg",
+  avatarWebp: "https://assets.chanhdai.com/images/chanhdai-avatar-ghibli.webp",
+  ogImage: "https://assets.chanhdai.com/images/screenshot-og-image-dark.png",
+  keywords:
+    "ncdai, nguyenchanhdai, nguyen chanh dai, chanhdai, chanh dai, iamncdai, quaric, zadark, nguyễn chánh đại, chánh đại",
+  dateCreated: "2023-10-20", // YYYY-MM-DD
+};
